20491a936adaf4ab64fa7ac80b7025e95d4372ec
Unknown <jerar.farah@wsu.edu>
2018-03-29 15:16:34 -0700

    UI Collection added

---
+++ Homework Buddy/ClassesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ClassesViewController.swift
+//  Homework Buddy
+//
+//  Created by Jiro Farah on 3/22/18.
+//  Copyright © 2018 Jiro Farah. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ClassesViewController: UICollectionViewController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
